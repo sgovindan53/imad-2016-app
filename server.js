aa467d7251b529ac140778083b3c639b2360bf36
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-09-22 18:21:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,17 +6,43 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-articleOne = {
-    title = 'Article One | Suthan Govindan',
-    heading = 'Article One',
-    date = 'Sep 5, 2016',
-    content = `<p>
+'article-one': {           //see remarks at line 89
+    title: 'Article One | Suthan Govindan',
+    heading: 'Article One',
+    date: 'Sep 5, 2016',
+    content: `<p>
                     This is the content for my first article. This is the content for my first article.
                     This is the content for my first article. This is the content for my first article.
                     This is the content for my first article. This is the content for my first article.
-                </p>
-                <p>
+               </p>
+               <p>
+                    This is the content for my first article. This is the content for my first article.
+                    This is the content for my first article. This is the content for my first article.
+                    This is the content for my first article. This is the content for my first article.
+               </p>
+               <p>
+                    This is the content for my first article. This is the content for my first article.
+                    This is the content for my first article. This is the content for my first article.
+                    This is the content for my first article. This is the content for my first article.
+               </p>`
 },
+'article-two': {                      //see remarks at line 89
+    title: 'Article Two | Suthan Govindan',
+    heading:'Article Two',
+    date: 'Sep 10, 2016',
+        content: `<p>
+                        This is the content for my second article. 
+                   </p>`
+},
+'article-three': {                      //see remarks at line 89
+    title: 'Article Three | Suthan Govindan',
+    heading: 'Article Three',
+    date: 'Sep 15, 2016',
+        content: `<p>
+                        This is the content for my third article. 
+                   </p>`
+},
+};
 
 function createTemplate (data) {
     var title = data.title;
@@ -57,17 +83,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/articleName', function (req, res) {
+    // articleName == article-one
+    //articles[articleName] == () content object for article one
+    //object names at lines 9, 29, 37 are changed to match with article-one etc & put in quotes
+    //to extract the articleName value, the following line is added, a functionality of 'express' framework
+    var articleName = req.params.articleName;
   res.send(createTemplate(articleOne));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
