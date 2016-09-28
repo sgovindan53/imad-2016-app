9feb9372c775cf16923f21cc51689b99e126cf65
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-09-28 14:20:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 // Counter code
-var button = document.getElementbyId("counter");
+var button = document.getElementbyId('counter');
 var counter =0;
 button.onclick = function() {
     
@@ -9,7 +9,7 @@ button.onclick = function() {
     
     //render the variable in the correct span
     counter = counter + 1;
-    var span = document.getElementbyId("count");
+    var span = document.getElementbyId('count');
     span.innerHTML = counter.toString();
     
 };
