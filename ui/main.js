ff6e6a1cb2d22d5abc1a039a92f7d7964e78d0a7
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-10-03 09:24:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,44 +1,16 @@
 // Counter code
 var button = document.getElementbyId('counter');
-
+var counter = 0;
 button.onclick = function() {
     
     //create a request object
-    var request = XMLHttpRequest();
     
     //capture the response and store it in our variable
-    request.onreadystatechange = function() {
-       if (request.readystate === XMLHttpRequest.DONE) {
-             //TAKE SOME ACTION
-            if (request.status ===200) {
-                var counter = request.responseText;
-                var span = document.getElementbyId('count');
-                span.innerHTML = counter.toString();
+    
+    
+    //Render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementbyId('count');
+    span.innerHTML = counter.toString();
                 
-              }
-            }
-           
-       };
-  // make the request
-  request.open('GET', 'http://sgovindan53.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
-    
-//submit name
-    var nameInput = document.getElementbyId("name");
-    var name = nameInput.value;
-    var submit = document.getElementbyId('submit_btn');
-    // go to index file and create id for submit_btn
-    submit.onclick = function() {
-    //Make a request to the server and send the name
-    
-    //Capture a list of names and render it as per list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for (var i=0; i<names.length;i++) {
-        list == '<li>' +names[i]+ '</li>';
-    }
-    var ul= document.getElementbyId('namelist');
-    ul.innerHtml = list;
-  }
-};
+}; 
