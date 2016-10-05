2a885b8ca36f4aa9165b1a2efa89fa7dd95bb835
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-10-05 22:35:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,7 @@ button.onclick = function() {
 }; 
 
 //submit name
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
+
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
     // Make a request to the server and send the name
@@ -52,6 +51,8 @@ submit.onclick = function() {
        }
     };
    // Make the request 
+  var nameInput = document.getElementById("name");
+  var name = nameInput.value;
   request.open('GET', 'http://sgovindan53.imad.hasura-app.io/submit-name?name' + name, true);
   request.send(null);
                 
