074bf784b20d41b24c044c8d129c7b94c876749a
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-11-03 09:02:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('/submit-name', function(req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
     
-    var articleName = req.params.articleName;
+ //   var articleName = req.params.articleName;
   
  //why ' and " around teh params portion; if single quote not given, the code interprets article-one as article minus one and error 
  // message is displayed. (Try running this code without the single quote)
@@ -138,8 +138,7 @@ app.get('/articles/:articleName', function (req, res) {
           if (result.rows.length===0){
               res.status(404).send('Article not found');
               
-            }
-              else {
+            }             else {
                   var articleData = result.rows(0);
                   res.send(createTemplate(articleData));
                   }
