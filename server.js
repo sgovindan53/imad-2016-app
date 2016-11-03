8316cf06a0896312f42bfbcd68c6063d5f9a2848
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-11-03 16:55:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.get('/submit-name', function(req, res) {
 app.get('/articles/:articleName', function (req, res) {
     
 // pool.query("SELECT * FROM article WHERE title = '+ req.params.articleName '", function (err, result) {
-pool.query("SELECT * FROM article WHERE title = article-one", function (err, result) {
+pool.query("SELECT * FROM article WHERE title = 'article-one'", function (err, result) {
    if (err) {
        res.status(500).send(err.toString());
    } else {
