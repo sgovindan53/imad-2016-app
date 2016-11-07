aee86b49d624375be5583928a16b4175432eedcd
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-11-07 12:56:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,13 +123,13 @@ app.get('/submit-name', function(req, res) {
     //JSON - javascript object notation
     res.send(JSON.stringify(names)); 
 });
-var pool = new Pool(config);
+
 app.get('/articles/:articleName', function (req, res) {
  
 //  pool.query("SELECT * FROM article WHERE title=$1", [req.params.articleName], function(err,result) {  
 // pool.query("SELECT * FROM article WHERE title ='"+req.params.articleName+"'", function (err, result) {
 //pool.query("SELECT * FROM article WHERE title = "'article-one'"", function (err, result) {
- pool.query("SELECT * FROM article WHERE title ='"+req.params.articleName+"'", function (err, result) {
+ pool.query("SELECT * FROM article WHERE title ='+req.params.articleName+'", function (err, result) {
    if (err) {
        res.status(500).send(err.toString());
    } else {
