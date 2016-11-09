4f8260ae0e33d54c28efb075be7acafc69b994b8
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-11-09 15:15:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ function hash(input, salt) {
 }
 app.get('/hash/:input', function (req, res) {
    // var hashedString = hash(req,params,input, salt);
-    var hashedString = hash(req,params,input, this-is-some-random-string);
+    var hashedString = hash(req,params,input, 'this-is-some-random-string');
     res.send(hashedString);
   
   
