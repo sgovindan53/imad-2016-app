011879f8bb1a5282f9dc4e9560e409e36ba6a705
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-11-11 23:01:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,36 +1,10 @@
-// Counter code
-var button = document.getElementById('counter');
 
-button.onclick = function() {
-    
-    //create a request object
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it in our variable
-    request.onreadystatechange = function () {
-       if (request.readyState === XMLHttpRequest.DONE) {
-         //TAKE SOME ACTION
-        if (request.status ===200) {
-        var counter = request.responseText;
-        
-        var span = document.getElementById('count');
-        span.innerHTML = counter.toString();
-        //counter=counter+1;
-        }
-       }
-    };
-   // Make the request 
-  request.open('GET', 'http://sgovindan53.imad.hasura-app.io/counter', true);
-  request.send(null);
-                
-}; 
+//submit username, password to login
 
-//submit name
 
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
-    // Make a request to the server and send the name
-    //create a request object
+   
     var request = new XMLHttpRequest();
     
     //capture the response and store it in our variable
@@ -38,22 +12,22 @@ submit.onclick = function() {
        if (request.readyState === XMLHttpRequest.DONE) {
          //TAKE SOME ACTION
         if (request.status ===200) {
-        var names = request.responseText;
-        names = JSON.parse(names);
-        var list='';
-        for (var i=0; i<names.length; i++) {
-            list += '<li>' + names[i] + '</li>';
-        }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
-        
+        console.log("User logged in");
+        alert("Logged in successfully");
+       } else if (request.status===403) {
+           alert("Username/password is incorrect");
+        } else if (request.status===500) {
+            alert("Something went wrong on the server!");
         }
        }
     };
    // Make the request 
-  var nameInput = document.getElementById("name");
-  var name = nameInput.value;
-  request.open('GET', 'http://sgovindan53.imad.hasura-app.io/submit-name?name=' + name, true);
-  request.send(null);
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  console.log("username");
+  console.log("password");
+  request.open('POST', 'http://sgovindan53.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify<{username: username, password: password}>);
                 
 }; 
