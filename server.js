e62a65fd5ce8cabc632567f8fb98bd58a2a0de7d
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-11-12 09:48:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/hash/:input', function (req, res) {
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
-    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)' [username, dbstring] function (err, result) {
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)' [username,dbstring] function (err, result) {
      if(err) { 
        res.status(500).send(err.toString());
    }
