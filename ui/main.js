f38140d6675a6d3df013efc31f92c89d79374431
SUTHAN GOVINDAN <sgovindan53@gmail.com>
2016-11-12 09:48:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,6 +28,6 @@ submit.onclick = function() {
   console.log("password");
   request.open('POST', 'http://sgovindan53.imad.hasura-app.io/login', true);
   request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.stringify<{username: username, password: password}>);
+  request.send(JSON.stringify(username:username,password:password);
                 
 }; 
